9e4a5b633acf054456582807ccbee097deb3f94a
tuansydau <tukauu@gmail.com>
2025-03-04 20:38:05 -0500

    rename: webserver.c -> webserver, add: listen and accept

--- web_server.c
+++ web_server.c
@@ -24,32 +24,50 @@ void error(const char *msg) {
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     fprintf(stderr, "Port number not provided, program terminated.");
+    fputs("\n", stderr);
     exit(1);
   }
 
   int sockfd, newsockfd, portno, n;
-  char buffer[255];
-
+  char buffer[256];
   struct sockaddr_in serv_addr, cli_addr;
-  socklen_t clilen;
+  socklen_t cli_len;
 
+  // 1. Create a socket
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd < 0) {
     error("Error opening socket");
   }
 
-  bzero((char *) &serv_addr, sizeof(serv_addr));
+  bzero((char *)&serv_addr, sizeof(serv_addr));
   portno = atoi(argv[1]);
   printf("PORT: %d\n", portno);
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(portno);
   serv_addr.sin_addr.s_addr = INADDR_ANY;
-  
-  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))){
+
+  // 2. Bind socket to a port
+  if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)))
     error("Binding failed.");
-  }
-  
+
   printf("Binding succeeded.");
+
+  // 3. Listen for connections
+  listen(sockfd, 5);
+
+  // 4. Accept a connection
+  cli_len = sizeof(cli_addr);
+  newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &cli_len);
+  if (newsockfd < 0)
+    error("Error on accept");
+
+  // 5. Read request
+  bzero(buffer, sizeof(buffer));
+  n = read(newsockfd, buffer, 256);
+  if (n < 0)
+    error("Error on read");
+
+  printf("client sent: %s", buffer);
 
   printf("\n");
 }
