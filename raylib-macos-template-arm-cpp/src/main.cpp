e5dfc74c094477dce93a40749f57e8703f912868
tuansydau <tukauu@gmail.com>
2025-03-08 19:52:53 -0500

    add: raylib template game

---
+++ raylib-macos-template-arm-cpp/src/main.cpp
@@ -0,0 +1,21 @@
+#include <raylib.h>
+#include <raymath.h>
+#include "view.h"
+
+int main()
+{
+    // Initial window viewport setup. Check View for scaling
+    View Viewport;
+    InitWindow(Viewport.Width, Viewport.Height, "A New Window");
+    SetTargetFPS(120);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
