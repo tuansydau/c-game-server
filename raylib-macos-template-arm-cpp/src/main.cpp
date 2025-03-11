95421104c3538355ef62a3d470be0d7fdd20bf3b
tuansydau <tukauu@gmail.com>
2025-03-11 06:58:08 -0400

    add: user can control a ball on the screen

--- raylib-macos-template-arm-cpp/src/main.cpp
+++ raylib-macos-template-arm-cpp/src/main.cpp
@@ -8,11 +8,26 @@ int main()
     View Viewport;
     InitWindow(Viewport.Width, Viewport.Height, "A New Window");
     SetTargetFPS(120);
+    int ballX = Viewport.Center.X;
+    int ballY = Viewport.Center.Y;
 
     while (!WindowShouldClose())
     {
+        if (IsKeyDown(KEY_RIGHT))
+            ballX += 3;
+        else if (IsKeyDown(KEY_LEFT))
+            ballX -= 3;
+        if (IsKeyDown(KEY_UP))
+            ballY -= 3;
+        else if (IsKeyDown(KEY_DOWN))
+            ballY += 3;
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        DrawCircle(ballX, ballY, 20, BLACK);
+
+        // Here is where I would update the server on where the client ball is
+
         EndDrawing();
     }
 
